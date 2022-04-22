8579657e794fb827686f6a3d1af5de674c24c338
zhangyingshen <yszhang@macauisoftstone.com>
2022-04-22 11:26:56 +0800

    add

---
+++ src/main/java/com/eason/demo/common/Constants.java
@@ -0,0 +1,16 @@
+package com.eason.demo.common;
+
+/**
+ * Created by eason on 2022/4/22.
+ */
+public class Constants {
+    /**
+     * 销售策略类型
+     */
+    public interface SaleStrategyType {
+        public static final String NORMAL = "noraml";
+        public static final String DISCOUNT = "discount";
+        public static final String OVEROFF = "overoff";
+    }
+
+}
